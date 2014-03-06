fcd7ad1084e0811f3c49ada2c0acb36f5f25f697
James Hunter <james.hunter@me.com>
2014-03-06 17:42:43 +0000

    Fiddled with image loading

--- app/models/snap.rb
+++ app/models/snap.rb
@@ -1,5 +1,5 @@
 class Snap < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :convert_options => { :all => '-auto-orient' }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   belongs_to :user
